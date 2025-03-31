80ccfa075ca2b0909f510244ed2fc918a1780be1
JL1704 <joseluiscg170404@gmail.com>
2025-03-30 19:03:03 -0600

    init: inicializar proyecto PlateScan
    Configuración inicial del proyecto, incluyendo la estructura de carpetas y archivos básicos.

---
+++ app/src/main/java/com/deltasquad/platescanapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.deltasquad.platescanapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.deltasquad.platescanapp.ui.theme.PlateScanAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlateScanAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PlateScanAppTheme {
+        Greeting("Android")
+    }
+}
