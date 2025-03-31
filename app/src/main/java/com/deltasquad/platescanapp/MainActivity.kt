69f649038b7a7a503f5c5b8393baba30270f5681
JL1704 <joseluiscg170404@gmail.com>
2025-03-30 19:51:04 -0600

    style: declaracion de dimensiones para PlateScanApp

--- app/src/main/java/com/deltasquad/platescanapp/MainActivity.kt
+++ app/src/main/java/com/deltasquad/platescanapp/MainActivity.kt
@@ -6,29 +6,40 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.deltasquad.platescanapp.ui.theme.PlateScanAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PlateScanAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            val windowSize = calculateWindowSizeClass(activity = this)
+            PlateScanAppTheme(
+                windowSize = windowSize.widthSizeClass
+            ) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
--- app/src/main/java/com/deltasquad/platescanapp/ui/theme/Theme.kt
+++ app/src/main/java/com/deltasquad/platescanapp/ui/theme/Theme.kt
@@ -8,8 +8,17 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = primaryGreen,
@@ -28,11 +37,23 @@ private val LightColorScheme = lightColorScheme(
 
 )
 
+private val LocalDimens = staticCompositionLocalOf { DefaultDimens }
+
+@Composable
+fun ProvideDimens(
+    dimens: Dimens,
+    content: @Composable () -> Unit
+){
+    val dimensionSet = remember { dimens }
+    CompositionLocalProvider(LocalDimens provides dimensionSet, content = content)
+}
+
 @Composable
 fun PlateScanAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    windowSize: WindowWidthSizeClass = WindowWidthSizeClass.Compact,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -44,10 +65,33 @@ fun PlateScanAppTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    val view = LocalView.current
+    if(!view.isInEditMode){
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    val dimensions = if(windowSize > WindowWidthSizeClass.Compact)
+        TabletDimens
+    else
+        DefaultDimens
+
+    ProvideDimens(dimens = dimensions) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+
+}
+
+object PlateScanAppTheme{
+    val dimens: Dimens
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalDimens.current
 }
