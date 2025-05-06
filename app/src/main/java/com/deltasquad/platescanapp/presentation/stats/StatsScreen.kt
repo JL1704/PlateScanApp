849cfba528318070d88cc3fe161cdb5e847972d0
RobDav22 <robertodavila735@gmail.com>
2025-05-05 22:07:33 -0600

    feat: se creo el componente StatCard

--- app/src/main/java/com/deltasquad/platescanapp/presentation/stats/StatsScreen.kt
+++ app/src/main/java/com/deltasquad/platescanapp/presentation/stats/StatsScreen.kt
@@ -1,22 +1,32 @@
 package com.deltasquad.platescanapp.presentation.stats
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.deltasquad.platescanapp.presentation.components.*
 import coil.compose.AsyncImage
+import com.deltasquad.platescanapp.R
 import com.deltasquad.platescanapp.presentation.theme.PlateScanAppTheme
 
 @Composable
-fun StatsScreen(){
+fun StatsScreen(onBackClick: () -> Unit = {}) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
