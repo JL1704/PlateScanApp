1a480a747678533e0ec43c54215430e40052bfd3
RobDav22 <robertodavila735@gmail.com>
2025-05-05 22:10:07 -0600

    feat: se modifico la pantalla de Stats

--- app/src/main/java/com/deltasquad/platescanapp/presentation/stats/StatsScreen.kt
+++ app/src/main/java/com/deltasquad/platescanapp/presentation/stats/StatsScreen.kt
@@ -10,10 +10,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,26 +19,45 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.deltasquad.platescanapp.presentation.components.*
-import coil.compose.AsyncImage
 import com.deltasquad.platescanapp.R
 import com.deltasquad.platescanapp.presentation.theme.PlateScanAppTheme
 
 @Composable
 fun StatsScreen(onBackClick: () -> Unit = {}) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(text = "Esta es la pantala stats")
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(128.dp))
-
-        AsyncImage(
-            model = "https://cdn-icons-png.flaticon.com/512/8462/8462143.png",//"https://via.placeholder.com/150",
-            contentDescription = "Work in progress",
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .size(300.dp)
+                .fillMaxWidth()
+                .clickable { onBackClick() }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back_24),
+                contentDescription = "Volver"
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Box(modifier = Modifier.fillMaxWidth()) {
+                SectionLabel(
+                    text = "Stats",
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+        StatCard(
+            title = "Escaneos por día",
+            content = "Hoy: 23 escaneos"
+        )
+
+        StatCard(
+            title = "Reportes Mensuales",
+            content = "Este mes: 12 reportes"
         )
     }
 }
