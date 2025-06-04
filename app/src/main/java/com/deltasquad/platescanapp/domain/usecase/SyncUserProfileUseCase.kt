460698bbb39067b454168663e046b7b7f18b0f16
JL1704 <joseluiscg170404@gmail.com>
2025-06-03 18:51:30 -0600

    refactor: se elimino codigo no utilizado

--- app/src/main/java/com/deltasquad/platescanapp/domain/usecase/SyncUserProfileUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.deltasquad.platescanapp.domain.usecase
-
-import com.deltasquad.platescanapp.data.repository.ProfileRepository
-
-class SyncUserProfileUseCase(private val repository: ProfileRepository) {
-    suspend operator fun invoke() {
-        repository.createUserProfileIfNotExists()
-    }
-}
